ce171dce164e24a8dc83838c50b1b8d669348c73
Piyush Mahajan <piyushmahajan077@gmail.com>
2025-12-22 01:01:52 +0530

    Add global request logger middleware

--- Thunder/server/__init__.py
+++ Thunder/server/__init__.py
@@ -3,7 +3,13 @@
 from aiohttp import web
 from .stream_routes import routes
 
+
+@web.middleware
+async def request_logger_middleware(request, handler):
+    print(f"DEBUG: Incoming Request: {request.method} {request.path}")
+    return await handler(request)
+
 async def web_server():
-    web_app = web.Application(client_max_size=30000000)
+    web_app = web.Application(client_max_size=30000000, middlewares=[request_logger_middleware])
     web_app.add_routes(routes)
     return web_app
